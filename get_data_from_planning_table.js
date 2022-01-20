5e016bc2a0225d76e70dc853880864829ae556de
Shashank Raj <shashank1_theone@yahoo.co.in>
2022-01-20 19:01:01 +0530

    added code to get data from planning table

---
+++ get_data_from_planning_table.js
@@ -0,0 +1,10 @@
+/// Apply the value in the PLanning Table using
+
+Table.getPLanning().setUserInput(user_input)
+
+// the userinput can be from a Selection or from a checkbox.
+// Then use :
+
+Table.getDataSource().getResultset() // to get all the resultset in an array.
+
+// Then we can filter for the value that we need. 
